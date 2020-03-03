f4b68135e0acf485fca0a161b965a03586def7ea
CVBirdNight724 <61728000+CVBirdNight724@users.noreply.github.com>
2020-03-03 17:24:54 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,11 +3,10 @@ namespace Mecanum{
     let m1B: number
     let m2A: number
     let m2B: number
-    export function setMotor(M1A: DigitalPin, M1B: DigitalPin,
-        M2A: DigitalPin, M2B: DigitalPin): void{
-            m1A = M1A
-            m1B = M1B
-            m2A = M2A
-            m2B = M2B
+    export function setMotor(M1A: DigitalPin, M1B: DigitalPin, M2A: DigitalPin, M2B: DigitalPin): void{
+        m1A = M1A
+        m1B = M1B
+        m2A = M2A
+        m2B = M2B
     }
 }
